d1e9ce68b524a524d53c4ba41dab2313ef311051
nataliaaucopsi <nataliaauco.psico@gmail.com>
2026-07-10 20:37:46 -0300

    Update vite.config.ts
    base: '/Natalia-Auco-Psicologa/',

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Natalia-Auco-Psicologa/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
